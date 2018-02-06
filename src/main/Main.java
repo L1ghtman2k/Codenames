23d08ee5f0404122305df3e09be21f1f11272473
ramar <ramar@LAPTOP-DGJNV155>
2018-02-05 21:02:50 -0500

    Hello

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("I like eating tide pods");
+		System.out.println("I like eating tid    e pods");
 	}
 
 }
