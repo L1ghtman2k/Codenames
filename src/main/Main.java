9fb4e2223388b2e2d1abf83a91b89119a613adaf
Unknown <Unknown@DESKTOP-IJRQ5LJ>
2018-02-05 18:00:42 -0800

    dsaads

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("I mkdas;lkkdsal;kl;daskld;saldsasaddsadsa");
+		System.out.println("I mkdas;lkkdsadsadasdasl;kl;daskld;saldsasaddsadsa");
 	}
 
 }
