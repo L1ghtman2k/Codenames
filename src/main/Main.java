e3bd8e87ea1ea115f40edb27f0ff0ee01a40e4c8
Unknown <Unknown@DESKTOP-IJRQ5LJ>
2018-02-05 18:05:22 -0800

    dsa

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("I mkdas;lkkdsadsadasdasl;d;saldsasaddsadsa");
+		System.out.println("I mkdas;asdasl;d;saldsasadsa");
 	}
 
 }
