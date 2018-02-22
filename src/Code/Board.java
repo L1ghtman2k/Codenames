c4761336683d8b00418584f0c31024e14acddf9a
Aibek <aibek.zhil@gmail.com>
2018-02-22 13:38:49 -0500

    Adding more method to Board and Location

--- src/Code/Board.java
+++ src/Code/Board.java
@@ -5,36 +5,55 @@ import java.util.ArrayList;
 public class Board {
 	private Location[][] grid;
 	private ArrayList<String> allNames;
-	private String[] randomCodenames;
+	private String[] allCodenames;
+
+	public Board() {
+		grid = new Location[5][5];
+		allCodenames = new String[25];
+	}
+
 	private ArrayList<String> allRoles = new ArrayList<String>() {{
 		////////////////////////8 Blue Agents//////////////////////////////////////
-	    add("Blue Agent"); add("Blue Agent"); add("Blue Agent"); add("Blue Agent");
-	    add("Blue Agent"); add("Blue Agent"); add("Blue Agent"); add("Blue Agent");
-	    ///////////////////////9 Red agents////////////////////////////////////////
-	    add("Red Agent"); add("Red Agent"); add("Red Agent"); add("Red Agent"); add("Red Agent");
-	    add("Red Agent"); add("Red Agent"); add("Red Agent"); add("Red Agent");
-	    ///////////////////////7 Bystanders////////////////////////////////////////
-	    add("Bystander"); add("Bystander"); add("Bystander");
-	    add("Bystander"); add("Bystander"); add("Bystander"); add("Bystander");
-	    //////////////////////1 Assassin///////////////////////////////////////////
-	    add("Assassin");
+		add("Blue Agent"); add("Blue Agent"); add("Blue Agent"); add("Blue Agent");
+		add("Blue Agent"); add("Blue Agent"); add("Blue Agent"); add("Blue Agent");
+		///////////////////////9 Red agents////////////////////////////////////////
+		add("Red Agent"); add("Red Agent"); add("Red Agent"); add("Red Agent"); 
+		add("Red Agent"); add("Red Agent"); add("Red Agent"); add("Red Agent");
+		add("Red Agent");
+		///////////////////////7 Bystanders////////////////////////////////////////
+		add("Bystander"); add("Bystander"); add("Bystander");
+		add("Bystander"); add("Bystander"); add("Bystander"); 
+		add("Bystander");
+		//////////////////////1 Assassin///////////////////////////////////////////
+		add("Assassin");
 	}};
-	
-	
+
+
 	public ArrayList<String> retAllCodenames(String filename){
-		
+
 		return null;
 	}
-	
+
 	public ArrayList<String> ret25RandomCodenames(ArrayList<String> wordStrorage){
-		
+
 		return null;
 	}
+
+	public void assignLocationToGrid(){
+
+	}
+
+
 	
-	public Location[][] assignLocationToGrid(){
-		
-		
-		return grid;
+	public Location[][] getGrid() {
+		return grid;	
+	}
+	public ArrayList<String> getAllNames() {
+		return allNames;	
+	}
+	public String[] getAllCodenames() {
+		return allCodenames;	
 	}
 	
+
 }
--- src/Code/Location.java
+++ src/Code/Location.java
@@ -4,4 +4,24 @@ public class Location {
 	private String role;
 	private String codename;
 	private boolean revealed;
+	
+	
+	public Location(String role, String codename, boolean revealed) {
+		this.role = role;
+		this.codename = codename;
+		this.revealed = revealed;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public String getCodename() {
+		return codename;
+	}
+
+	public boolean isRevealed() {
+		return revealed;
+	}
+
 }
