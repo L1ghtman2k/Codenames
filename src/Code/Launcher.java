67be280c990353be4983449540706885512d5457
Unknown <Unknown@DESKTOP-IJRQ5LJ>
2018-02-22 17:22:50 -0800

    Added Louncher

---
+++ src/Code/Launcher.java
@@ -0,0 +1,17 @@
+package Code;
+
+public class Launcher {
+	private Board board;
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	
+	public void StartTheGame() {
+		
+	}
+}
