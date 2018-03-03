2497dad632fbe4bd7cdaabda17ec2fa8e13e9145
green <green@polo-surface>
2018-03-03 15:36:04 -0500

    YEET

--- src/Code/Team.java
+++ src/Code/Team.java
@@ -9,41 +9,61 @@ public class Team {
 	private int count;
 	private boolean RevealedAssassin;
 	private Roles agents = Roles.Red;
-	 
+	
 	public Team(String TeamName) {
 		this.setTeamName(TeamName);
 		RevealedAssassin = false;
 	}
-	//Lowers the count by 1
+	/**
+	 * Lowers the count by 1.
+	 */
 	public void decrementCount() {
 		setCount(getCount() - 1);
 	}
 	
-	//Getters and Setters
+	/**
+	 * Returns the count.
+	 * @return count.
+	 */
 	public int getCount() {
 		return count;
 	}
-
+	/**
+	 * Sets the count.
+	 * @param count - int.
+	 */
 	public void setCount(int count) {
 		this.count = count;
 	}
-
+	/**
+	 * Returns the team's name
+	 * @return TeamName - String of team's name.
+	 */
 	public String getTeamName() {
 		return TeamName;
 	}
-
+	/**
+	 * Set's the team's name.
+	 * @param teamName - String to become the team's name.
+	 */
 	public void setTeamName(String teamName) {
 		TeamName = teamName;
 	}
-
+	/**
+	 * Returns if the assassin has been revealed or not.
+	 * @return true if assassin has been revealed, false otherwise.
+	 */
 	public boolean isRevealedAssassin() {
 		return RevealedAssassin;
 	}
-
+	/**
+	 * Sets whether or not the assassin has been revealed.
+	 * @param revealedAssassin - boolean.
+	 */
 	public void setRevealedAssassin(boolean revealedAssassin) {
 		RevealedAssassin = revealedAssassin;
 	}
-
+	
 	public Roles getAgents() {
 		return agents;
 	}
@@ -54,3 +74,4 @@ public class Team {
 	
 	
 }
+
