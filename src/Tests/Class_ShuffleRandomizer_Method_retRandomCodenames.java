3d5c408c0e673094514734419004a5cb5de6abb5
Aibek <aibek.zhil@gmail.com>
2018-03-03 03:09:22 -0500

    Finalizer retRandomCodenames

--- src/Tests/Class_ShuffleRandomizer_Method_retRandomCodenames.java
+++ src/Tests/Class_ShuffleRandomizer_Method_retRandomCodenames.java
@@ -17,28 +17,21 @@ import org.junit.Test;
 
 import Code.Board;
 import Code.Launcher;
-import Code.LocalFileExtension;
-import Code.ShuffleRandomizer;
 import Code.Location;
 
 public class Class_ShuffleRandomizer_Method_retRandomCodenames {
 	
 	Launcher launcher;
 	Board board;
-	ShuffleRandomizer shuffleRand;
-	LocalFileExtension extension;
 	
-//	@Before
-//	  public void setUp() {
-//		launcher = new Launcher();
-//		board = launcher.StartTheGame("Blue", "Red");
-//	//Delete This, check for Custom, Check for Gameword2, CheckforNull
-//	}
+	@Before
+	public void setup() {
+		launcher = new Launcher();
+	}
 	
 	@Test
 	public void WordStorageIsLessThanTotalLocations_returnNull() {
-		Launcher launch = new Launcher();
-		board = launch.StartTheGame("RedName", "BlueName");
+		board = launcher.StartTheGame("RedName", "BlueName");
 		List<String> mylist = Arrays.asList("A1", "Buffalo", "Knuckles");
 		List<String> assertedList = board.getRandomizer().retRandomCodenames(mylist);
 		assertNull(assertedList);
@@ -46,52 +39,36 @@ public class Class_ShuffleRandomizer_Method_retRandomCodenames {
 	
 	@Test
 	public void ProperWordStorage_returnListOfString_GameWords1() {
-	
-		Board newBoard = new Board(new Location[5][5], 9, 8, 1, "RedTeam", "BlueTeam");
-		shuffleRand = new ShuffleRandomizer(newBoard);
+		board = launcher.StartTheGame("RedName", "BlueName");
 		
-//		launcher = new Launcher();
-//		board = launcher.StartTheGame("RedTeam", "BlueTeam");
-		
-		
-		//board.setGrid(new Location[5][5]);
-		List<String> newList = shuffleRand.retRandomCodenames(newBoard.getExtension().retAllCodenames("src/GameWords1.txt"));
+		List<String> newList = board.getRandomizer().retRandomCodenames(board.getExtension().retAllCodenames("src/GameWords1.txt"));
 		assertNotNull(newList);
 		assertEquals(newList.size(), 25);
-		
 	}
 	
 	@Test
 	public void ProperWordStorage_returnListOfString_GameWords2() {
-		
-		Board newBoard = new Board(new Location[7][7], 9, 8, 1, "RedTeam", "BlueTeam");
-		shuffleRand = new ShuffleRandomizer(newBoard);
-		
-//		launcher = new Launcher();
-//		board = launcher.StartTheGame("RedTeam", "BlueName");
-//		
-//		board.setGrid(new Location[7][7]);
-		List<String> list = shuffleRand.retRandomCodenames(newBoard.getExtension().retAllCodenames("src/GameWords2.txt"));
+		board = launcher.StartTheGame(7,9,8,1,"RedName", "BlueName");
+		List<String> list = board.getRandomizer().retRandomCodenames(board.getExtension().retAllCodenames("src/GameWords2.txt"));
 		assertNotNull(list);
 		assertEquals(list.size(), 49);
 	}
-	
-	//Create two Board instances, 
-	//randomize their location using gamewords1.txt, 
-	//compare if locations are equal
+
 	@Test
 	public void CompareTwoDifferentBoards_returnDifferentBoards() {
-		Location[][] grid = new Location[5][5];
-		Board firstBoard = new Board(grid, 8, 9, 1, "RedTeam", "BlueTeam");
-		Board secondBoard = new Board(grid, 8, 9, 1, "RedTeam", "BlueTeam");
+		Launcher launcher1 = new Launcher();
+		Launcher launcher2 = new Launcher();
+		Board firstBoard = launcher1.StartTheGame("RedTeam", "BlueTeam");
+		Board secondBoard = launcher2.StartTheGame("RedTeam", "BlueTeam");
+		List<String> a = firstBoard.getRandomizer().retRandomCodenames(firstBoard.getExtension().retAllCodenames("src/GameWords1.txt"));
+		List<String> a2 = secondBoard.getRandomizer().retRandomCodenames(secondBoard.getExtension().retAllCodenames("src/GameWords1.txt"));
+		boolean checker = false;
 		
-//		firstBoard.getRandomizer().retRandomCodenames(board.getExtension().retAllCodenames("src/GameWords1.txt"));
-//		secondBoard.getRandomizer().retRandomCodenames(board.getExtension().retAllCodenames("src/GameWords1.txt"));
+		for(int i = 0; i < a.size(); i++)
+				if(!a.get(i).equals(a2.get(i)))
+					checker = true;			
+		assertTrue(checker);
 		
-		assertFalse(firstBoard.equals(secondBoard));
-		assertTrue(!firstBoard.equals(secondBoard));
-		assertNotNull(firstBoard);
-		assertNotNull(secondBoard);
 	}
 	
 	
