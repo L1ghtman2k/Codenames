975dfbe70682c64b2923f9a4a1476101c5a5fa51
green <green@polo-surface>
2018-03-03 15:36:42 -0500

    sdgfdsf

--- src/Code/Launcher.java
+++ src/Code/Launcher.java
@@ -2,11 +2,18 @@ package Code;
 
 public class Launcher {
 	private Board board;
- 
+	
+	/**
+	 * Returns the board being used.
+	 * @return current board.
+	 */
 	public Board getBoard() {
 		return board;
 	}
-
+	/**
+	 * Sets the board to be used.
+	 * @return new board.
+	 */
 	public void setBoard(Board board) {
 		this.board = board;
 	}
@@ -14,15 +21,15 @@ public class Launcher {
 	/**
 	 * This method returns Costume version of Board that contains n x n grid, with selected amount of Reds
 	 * selected amount of Blues selected amount Assassin and fills up the rest with Bystander,
-	 * and also assigns name to each Team
+	 * and also assigns name to each Team.
 	 * 
-	 * @param int n controls the dimensions of the grid(grid[n][n])
-	 * @param int Reds is the amount of Red Agents on the grid
-	 * @param int Blues is the amount of Blue Agents on the grid
-	 * @param int Assassins is the amount of Assassins on the grid
-	 * @param String RedName is the name of the Red Team
-	 * @param String BlueName is the name of the Blue Team
-	 * @return Returns a customized board
+	 * @param n - int that controls the dimensions of the grid(grid[n][n]).
+	 * @param Reds - int, amount of Red Agents on the grid.
+	 * @param Blues - int, amount of Blue Agents on the grid.
+	 * @param Assassins - int, amount of Assassins on the grid.
+	 * @param RedName - String, name of the Red Team.
+	 * @param BlueName - String, name of the Blue Team.
+	 * @return Returns a customized board.
 	 */
 	public Board StartTheGame(int n, int Reds, int Blues, int Assassins, String RedName, String BlueName) {
 		Location[][] grid = new Location[n][n];
@@ -41,9 +48,9 @@ public class Launcher {
 	 * This method returns Default version of Board that contains 5 x 5 grid, with 9 Reds,
 	 * 8 Blues, 1 Assassin, and 1 Bystander, and also assigns name to each Team
 	 * 
-	 * @param String RedName - name of Red team	
-	 * @param String BlueName - name of Blue team
-	 * @return Default board
+	 * @param RedName - String, name of Red team.	
+	 * @param BlueName - String, name of Blue team.
+	 * @return Default board.
 	 */
 	public Board StartTheGame(String RedName, String BlueName) {
 		board = StartTheGame(5, 9, 8, 1, RedName, BlueName);	
